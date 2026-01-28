c99673e5a9e449ccb3665b1ca4679c0c6e2bdaf2
rajurao <rajurao1107@gmail.com>
2026-01-28 22:43:48 +0530

    first commit

--- models/User.js
+++ models/User.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }, // Hashed password store hoga
 });
 
-export default mongoose.models.User ||
-  mongoose.model("User", userSchema);
+export default mongoose.models.authentication || mongoose.model("authentication", userSchema);
